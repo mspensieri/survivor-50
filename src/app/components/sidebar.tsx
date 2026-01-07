b235ffa91e8a471dd61a26b0efe1f7f4d41ffa67
Mike Spensieri <mikespensieri@smooch.io>
2026-01-06 20:56:53 -0500

    Parametrize ratings strings

--- src/app/components/sidebar.tsx
+++ src/app/components/sidebar.tsx
@@ -39,6 +39,61 @@ const styles: Record<string, React.CSSProperties> = {
   },
 };
 
+function stars(rating: number) {
+  return "★".repeat(rating) + "☆".repeat(5 - rating);
+}
+
+function ratingDescription(rating: number) {
+  switch (rating) {
+    case 1:
+      return "No significant placements yet";
+    case 2:
+      return "Finished in the top 50% in a past season";
+    case 3:
+      return "Finished in the top 10 in a past season";
+    case 4:
+      return "Finished in the top 3 in a past season";
+    case 5:
+      return "Winner of a past season";
+    default:
+      return "";
+  }
+}
+
+function ratingTitle(rating: number) {
+  switch (rating) {
+    case 1:
+      return "Rookie";
+    case 2:
+      return "Apprentice";
+    case 3:
+      return "Contender";
+    case 4:
+      return "Expert";
+    case 5:
+      return "Champion";
+    default:
+      return "";
+  }
+}
+
+function ratingSummary(rating: number) {
+  switch (rating) {
+    case 1:
+      return "Bottom 50%";
+    case 2:
+      return "Top 50%";
+    case 3:
+      return "Top 10";
+    case 4:
+      return "Top 3";
+    case 5:
+      return "Winner";
+    default:
+      return "";
+  }
+}
+
 export default function Sidebar(props: {
   shown: boolean;
   onHide: () => void;
@@ -112,25 +167,29 @@ export default function Sidebar(props: {
         </div>
         <h2>History</h2>
         <div>
-          <span style={styles.emphasis}>Overall rating: </span>★★☆☆☆
-          (Apprentice)
+          <span style={styles.emphasis}>Overall rating: </span>
+          {stars(2)} ({ratingTitle(2)})
         </div>
-        <div style={styles.ratingDescription}>
-          Finished in the top 50% of teams in a past season
-        </div>
+        <div style={styles.ratingDescription}>{ratingDescription(2)}</div>
         <Table striped bordered style={styles.table}>
           <tbody>
             <tr>
               <td>Season 49</td>
-              <td>★☆☆☆☆ (Bottom 50%)</td>
+              <td>
+                {stars(1)} ({ratingSummary(1)})
+              </td>
             </tr>
             <tr>
               <td>Season 48</td>
-              <td>★★☆☆☆ (Top 50%)</td>
+              <td>
+                {stars(2)} ({ratingSummary(2)})
+              </td>
             </tr>
             <tr>
               <td>Season 47</td>
-              <td>★★★★☆ (Top 3)</td>
+              <td>
+                {stars(4)} ({ratingSummary(4)})
+              </td>
             </tr>
           </tbody>
         </Table>

d944da7afcbb146fe3165b19884f1b3d30b9b71a
Mike Spensieri <mikespensieri@smooch.io>
2026-01-08 16:13:04 -0500

    Use icons for player count

--- src/app/components/leaderboard.tsx
+++ src/app/components/leaderboard.tsx
@@ -10,6 +10,24 @@ import PlacementChart from "./placementChart";
 import Sidebar from "./sidebar";
 import { captainNames, stars } from "../utils/format";
 
+function UserIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      className="size-4"
+      style={{ display: "inline" }}
+    >
+      <path
+        fillRule="evenodd"
+        d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
+}
+
 const styles: Record<string, React.CSSProperties> = {
   indicatorGreen: {
     color: "var(--green-indicator-color)",
@@ -215,8 +233,9 @@ export default function Leaderboard(props: {
               <div style={styles.diff}>{getScoreDiff()}</div>
               <div style={styles.divider}></div>
               <div style={styles.playerCount}>
-                {activePlayers.length} active player
-                {activePlayers.length !== 1 ? "s" : ""}
+                {activePlayers.map((_, i) => (
+                  <UserIcon key={i} />
+                ))}
               </div>
               <div style={styles.placementHistory}>
                 <PlacementChart

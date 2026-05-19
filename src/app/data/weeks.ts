21ef1d502e111f0c5643300aa1590319cb7a02e1
Mike Spensieri <mspensieri@zendesk.com>
2026-05-19 13:16:10 -0400

    Week 12

--- src/app/data/weeks.ts
+++ src/app/data/weeks.ts
@@ -971,4 +971,45 @@ export const weeks: Array<Week> = [
       },
     },
   },
+  {
+    eliminated: [CIRIE, RICK],
+    [AUBRY]: {
+      [RuleSet.STANDARD]: {
+        survival: 1,
+        votes: 2,
+      },
+    },
+    [CIRIE]: {
+      [RuleSet.STANDARD]: {
+        survival: 1,
+        votes: 1,
+        placement: 2,
+      },
+    },
+    [JOE]: {
+      [RuleSet.STANDARD]: { survival: 1, votes: 2, individualImmunity: 2 },
+    },
+    [JONATHAN]: {
+      [RuleSet.STANDARD]: {
+        survival: 1,
+        votes: 2,
+      },
+    },
+    [RICK]: {
+      [RuleSet.STANDARD]: { survival: 1, placement: 2 },
+    },
+    [RIZO]: {
+      [RuleSet.STANDARD]: {
+        survival: 1,
+        votes: 2,
+      },
+    },
+    [TIFFANY]: {
+      [RuleSet.STANDARD]: {
+        survival: 1,
+        votes: 1,
+        individualImmunity: 2,
+      },
+    },
+  },
 ];
